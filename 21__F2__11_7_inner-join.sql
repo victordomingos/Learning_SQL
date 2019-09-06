2fc2d31e2db324908c9e4d8e8e088c8eefa90b40
victordomingos <editor.arcosonline@gmail.com>
2019-09-06 15:34:57 +0100

    21__F2__11_7_inner-join.sql

---
+++ 21__F2__11_7_inner-join.sql
@@ -0,0 +1,13 @@
+SELECT CM.data_hora, U.nome, E.especialidade_designacao
+
+FROM utente U
+    INNER JOIN consulta_medica CM
+        ON U.numero_utente = CM.utente
+    INNER JOIN medico M
+        ON CM.medico = M.numero_ordem
+    INNER JOIN especialidade E
+        ON M.especialidade = E.especialidade_codigo
+
+WHERE U.nome LIKE "%Santoro%"
+    AND CM.realizada = 1
+    AND E.especialidade_designacao LIKE "GINECOLOGIA";
