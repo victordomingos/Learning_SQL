5a6e2db99c9f34f8aeacce66d1d5dea0464779de
victordomingos <editor.arcosonline@gmail.com>
2019-09-10 12:03:23 +0100

    33__F2__14_13_max.sql

---
+++ 33__F2__14_13_max.sql
@@ -0,0 +1,20 @@
+-- Qual o médico que mais consultas deu?
+
+SELECT M.nome, COUNT(*) as total_consultas
+
+FROM medico M
+    INNER JOIN consulta_medica CM
+        on M.numero_ordem = CM.medico
+
+GROUP BY CM.medico
+HAVING total_consultas = (
+    SELECT COUNT(*) as total_consultas
+
+    FROM medico M
+        INNER JOIN consulta_medica CM
+            on M.numero_ordem = CM.medico
+
+    GROUP BY CM.medico
+    ORDER BY total_consultas DESC
+    LIMIT 1
+    );
